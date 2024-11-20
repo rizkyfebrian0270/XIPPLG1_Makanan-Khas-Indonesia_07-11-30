5f2815f63fff0aac34764e733d2823fcd668bb73
Rizki Febrian <541231191@student.smktelkom-pwt.sch.id>
2024-11-20 19:42:09 +0700

    install laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('login');
-});
+// Route::get('/', function () {
+//     return view('login');
+// });
+
+Route::get('/', [HomeController::class, "login"])->name('login');
+Route::get('/home', [HomeController::class, "index"])->name('homepage');
